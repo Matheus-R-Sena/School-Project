3c19de3ff19d95599745a637ab40eb7d6e95712e
Matheus da Rocha Sena <93345712+Matheus-R-Sena@users.noreply.github.com>
2024-12-17 22:19:08 -0300

    Adicionando alunoDAO pronto

--- aplicacaoMVC/src/java/model/AlunoDAO.java
+++ aplicacaoMVC/src/java/model/AlunoDAO.java
@@ -9,7 +9,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import entidade.Aluno;
-import entidade.Categoria;
 /**
  *
  * @author mathe
@@ -29,120 +28,117 @@ public class AlunoDAO implements Dao<Aluno> {
                 while (resultado.next()) {
                     aluno.setId(Integer.parseInt(resultado.getString("ID")));
                     aluno.setNome(resultado.getString("NOME"));
-                    aluno.setNome(resultado.getString("NOME"));
-                    aluno.setNome(resultado.getString("NOME"));
+                    aluno.setEmail(resultado.getString("EMAIL"));
+                    aluno.setCelular(resultado.getString("CELULAR"));
+                    aluno.setCpf(resultado.getString("CPF"));
+                    aluno.setSenha(resultado.getString("SENHA"));
+                    aluno.setEndereco(resultado.getString("ENDERECO"));
+                    aluno.setCidade(resultado.getString("CIDADE"));
+                    aluno.setBairro(resultado.getString("BAIRRO"));
+                    aluno.setCep(resultado.getString("CEP"));
                     
                 }
             }
         } catch (SQLException e) {
-            System.err.println("Query de select (get categoria) incorreta");
+            System.err.println("Query de select (get aluno) incorreta");
         } finally {
             conexao.closeConexao();
         }
-        return categoria;
+        return aluno;
     }
     
-}
-
-
-public class CategoriaDAO implements Dao<Categoria> {
-
     @Override
-    public Categoria get(int id) {
-        Conexao conexao = new Conexao();
-        Categoria categoria = new Categoria();
-        try {
-            PreparedStatement sql = conexao.getConexao().prepareStatement("SELECT * FROM Categorias WHERE ID = ? ");
-            sql.setInt(1, id);
-            ResultSet resultado = sql.executeQuery();
-
-            if (resultado != null) {
-                while (resultado.next()) {
-                    categoria.setId(Integer.parseInt(resultado.getString("ID")));
-                    categoria.setDescricao(resultado.getString("DESCRICAO"));
-                }
-            }
-        } catch (SQLException e) {
-            System.err.println("Query de select (get categoria) incorreta");
-        } finally {
-            conexao.closeConexao();
-        }
-        return categoria;
-    }
-
-    @Override
-    public void insert(Categoria t) {
+    public void insert(Aluno t) {
 
         Conexao conexao = new Conexao();
         try {
-            PreparedStatement sql = conexao.getConexao().prepareStatement("INSERT INTO Categorias (descricao) VALUES (?)");
-            sql.setString(1, t.getDescricao());
+            PreparedStatement sql = conexao.getConexao().prepareStatement("INSERT INTO alunos (nome, email, celular, cpf, senha, endereco, cidade, bairro, cep) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            sql.setString(1, t.getNome());
+            sql.setString(2, t.getEmail());
+            sql.setString(3, t.getCelular());
+            sql.setString(4, t.getCpf());
+            sql.setString(5, t.getSenha());
+            sql.setString(6, t.getEndereco());
+            sql.setString(7, t.getCidade());
+            sql.setString(8, t.getBairro());
+            sql.setString(9, t.getCep());
             sql.executeUpdate();
 
         } catch (SQLException e) {
-            System.err.println("Query de insert (categoria) incorreta");
+            System.err.println("Query de insert (Aluno) incorreta");
         } finally {
             conexao.closeConexao();
         }
     }
-
+    
     @Override
-    public void update(Categoria t) {
+    public void update(Aluno t) {
         Conexao conexao = new Conexao();
         try {
-            PreparedStatement sql = conexao.getConexao().prepareStatement("UPDATE Categorias SET descricao = ?  WHERE ID = ? ");
-            sql.setString(1, t.getDescricao());
-            sql.setInt(2, t.getId());
+            PreparedStatement sql = conexao.getConexao().prepareStatement("UPDATE alunos SET nome = ?, email = ?, celular = ?, cpf = ?, senha = ?, endereco = ?, cidade = ?, bairro = ?, cep = ? WHERE ID = ? ");
+            sql.setString(1, t.getNome());
+            sql.setString(2, t.getEmail());
+            sql.setString(3, t.getCelular());
+            sql.setString(4, t.getCpf());
+            sql.setString(5, t.getSenha());
+            sql.setString(6, t.getEndereco());
+            sql.setString(7, t.getCidade());
+            sql.setString(8, t.getBairro());
+            sql.setString(9, t.getCep());
+            sql.setInt(10, t.getId());
             sql.executeUpdate();
 
         } catch (SQLException e) {
-            System.err.println("Query de update (alterar categoria) incorreta");
+            System.err.println("Query de update (alterar Aluno) incorreta");
         } finally {
             conexao.closeConexao();
         }
     }
-
-    @Override
+    
+    
+     @Override
     public void delete(int id) {
         Conexao conexao = new Conexao();
         try {
-            PreparedStatement sql = conexao.getConexao().prepareStatement("DELETE FROM Categorias WHERE ID = ? ");
+            PreparedStatement sql = conexao.getConexao().prepareStatement("DELETE FROM alunos WHERE ID = ? ");
             sql.setInt(1, id);
             sql.executeUpdate();
 
         } catch (SQLException e) {
-            System.err.println("Query de delete (excluir categoria) incorreta");
+            System.err.println("Query de delete (excluir Aluno) incorreta");
         } finally {
             conexao.closeConexao();
         }
     }
     
-    @Override
+     @Override
     
     //Listar categorias 
-    public ArrayList<Categoria> getAll() {
+    public ArrayList<Aluno> getAll() {
 
-        ArrayList<Categoria> meusCategorias = new ArrayList();
+        ArrayList<Aluno> meusAlunos = new ArrayList();
         Conexao conexao = new Conexao();
         try {
-            String selectSQL = "SELECT * FROM Categorias";
+            String selectSQL = "SELECT * FROM alunos";
             PreparedStatement preparedStatement;
             preparedStatement = conexao.getConexao().prepareStatement(selectSQL);
             ResultSet resultado = preparedStatement.executeQuery();
             if (resultado != null) {
                 while (resultado.next()) {
-                    Categoria Categoria = new Categoria(
+                    Aluno Aluno = new Aluno(
                             resultado.getInt("ID"),
-                            resultado.getString("Descricao")
+                            resultado.getString("nome")
                     );
-                    meusCategorias.add(Categoria);
+                    meusAlunos.add(Aluno);
                 }
             }
         } catch (SQLException e) {
-            System.err.println("Query de select (GetAll - categorias) incorreta");
+            System.err.println("Query de select (GetAll - Alunos) incorreta");
         } finally {
             conexao.closeConexao();
         }
-        return meusCategorias;
+        return meusAlunos;
     }
 }
+
+
